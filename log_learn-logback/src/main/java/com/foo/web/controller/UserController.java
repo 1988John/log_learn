438c87638b677a65dbdffe05e203d0804fbafddf
jiangwang3 <jiangwang3@jd.com>
2018-02-02 09:15:20 +0800

    Initial commit

---
+++ log_learn-logback/src/main/java/com/foo/web/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.foo.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Description:
+ * Created by jiangwang3 on 2018/1/30.
+ */
+@Controller
+@RequestMapping(value = "/user")
+public class UserController {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping(value = "/info",method = RequestMethod.GET,
+            produces = {"application/json;charset=UTF-8"})
+    public @ResponseBody String showInfo(String name, Integer age){
+        Map<String,String> map = new HashMap<String, String>();
+        log.info("获取用户信息开始，name:{}，age:{}",name,age);
+        map.put("name",name);
+        map.put("age",age.toString());
+
+        return map.toString();
+    }
+}
---
+++ log_learn-logback/src/main/java/com/foo/web/domain/Person.java
@@ -0,0 +1,12 @@
+package com.foo.web.domain;
+
+import java.io.Serializable;
+
+/**
+ * Description:
+ * Created by jiangwang3 on 2018/2/1.
+ */
+public class Person implements Serializable{
+
+    private static final long serialVersionUID = -5380600864806462502L;
+}
